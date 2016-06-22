24003448c7cdea08b39c958970cb2fc948fd54f8
mjaorawala <mjaorawala@NZMJ01AKL>
2016-06-23 09:23:04 +1200

    Add project files.

---
+++ Models/UserModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Models
+{
+    public interface ITranslations
+    {
+        string this[string key] { get; }
+    }
+
+    public abstract class EmailModelBase : ITranslations
+    {
+        private Dictionary<string, string> _translations;
+
+        public string this[string key]
+        {
+            get { return _translations.ContainsKey(key) ? _translations[key] : string.Empty; }
+        }
+
+        public void AddTranslations(Dictionary<string, string> translations)
+        {
+            _translations = translations;
+        }
+
+        public ITranslations Translations => this;
+
+        public abstract string Component { get;  }
+        public abstract string Css { get;  }
+        public string Footer { get; set; }
+    }
+
+    public class UserModel : EmailModelBase
+    {
+        public UserModel()
+        {
+            ProductUrl = "http://example.com";
+            ProductDescription = "Expense Management";
+
+            AddTranslations(new Dictionary<string, string>
+            {
+                {"192", "Please click"},
+                {"193", "here"},
+                {"194", "to sign in to"},
+                {"223", "Welcome to"},
+                {"224", "Your company administrator has created a login for you to access"},
+                {"225", "Username:"},
+                {"386", "Password:"},
+                {"387", "Details relating to your password will be sent to you in a separate email."}
+            });
+
+            Footer = "This has a footer";
+        }
+
+        public string ProductUrl { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public override string Component => "Component_91";
+        public override string Css => "Bmo";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApplication.Models;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = new UserModel();
+       
+            var templatePaths = new[]
+            {
+                "EmailTemplates"
+            };
+
+            var templateServiceConfiguration = new TemplateServiceConfiguration
+            {
+                TemplateManager = new MyResolvePathTemplateManager(templatePaths),
+                Debug = true,
+                Namespaces = new HashSet<string> { "ConsoleApplication.Models", "System" }
+            };
+            
+            var service = RazorEngineService.Create(templateServiceConfiguration);
+
+            var key = service.GetKey("Body.cshtml");
+            var body = service.RunCompile(key, typeof(UserModel), model);
+
+            var pm = new PreMailer.Net.PreMailer(body);
+            
+            var result = pm.MoveCssInline(removeStyleElements: true, stripIdAndClassAttributes: true);
+
+            Console.WriteLine(result.Html);
+
+        }
+    }
+}
